e4fe28c9696f9bc953eab8cbdee36ecb140d8767
camekman <ekman.camilla@gmail.com>
2021-11-23 10:18:26 +0100

    first attempt with tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 const App = () => {
   return (
-    <div>
-      <h1>Hello!</h1>
+    <div className="flex justify-between my-4">
+      <h1 className="text-green-300">Hello!</h1>
       <h1>World!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
